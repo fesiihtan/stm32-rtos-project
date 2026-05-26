11f60f8f3031acd2098f4d5c7114a59a9bd8b74c
Fesih TAN <fesih67@outlook.com.tr>
2026-05-26 13:22:30 +0300

    add startup task and verify functionality

--- rtos-project/Config/FreeRTOSConfig.h
+++ rtos-project/Config/FreeRTOSConfig.h
@@ -78,7 +78,7 @@
  #define configRECORD_STACK_HIGH_ADDRESS            1
  #define configUSE_TRACE_FACILITY                   1
  #define configUSE_STATS_FORMATTING_FUNCTIONS       1
- #define configGENERATE_RUN_TIME_STATS              
+ #define configGENERATE_RUN_TIME_STATS              0
  extern void configureRunTime(void);
  #define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS()   configureRunTime()
  extern uint32_t getRunTimeCounter(void);
--- rtos-project/Core/Src/main.c
+++ rtos-project/Core/Src/main.c
@@ -8,13 +8,65 @@
 #include <stdint.h>
 #include "mcu.h"
 #include "FreeRTOSConfig.h"
+#include "FreeRTOSTasks.h"
+
+
+// Startup Task Prototype
+static void startup(void);
+
+// Run Time stats configuration prototype
+void configureRunTime(void);
+
+// Run Time stats get counter value prototype
+uint32_t getRunTimeCounter(void);
 
 
 int main(void)
 {
+
+  // Update the SystemCoreClock variable required by FreeRTOSConfig.h
+  SystemCoreClockUpdate();
+
+  // Create the startup task
+  startup();
+
+  // Start the FreeRTOS scheduler
+  vTaskStartScheduler();
+
+  
 	while(1)
 	{
 
 	}
 }
 
+/**
+ * @brief  Startup task used for the required FreeRTOS initializations on startup
+ */
+static void startup_task(void *param)
+{
+    // Delete the startup task
+    vTaskDelete(NULL);
+}
+
+/**
+ *  Creates the startup task.
+ */
+static void startup(void)
+{
+  configASSERT(pdPASS == xTaskCreate(startup_task, "Startup Task", STARTUP_TASK_STACK_SIZE, NULL, STARTUP_TASK_PRIORITY, NULL));
+}
+
+
+// Run Time stats configuration prototype
+void configureRunTime(void)
+{
+
+}
+
+
+// Run Time stats get counter value prototype
+uint32_t getRunTimeCounter(void)
+{
+  return 1;
+}
